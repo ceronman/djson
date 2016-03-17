376100028bb44a0166fa6e98a384da6787690012
Manuel Ceron <ceronman@gmail.com>
2016-03-17 23:20:48 +0100

    Tests now go in a different package. And added some integer tests.

--- decode_test.go
+++ decode_test.go
@@ -1,9 +1,20 @@
-package djson
+package djson_test
 
-import "testing"
+import (
+	"testing"
 
-func testDecodeBool(t *testing.T, input string, expected bool) {
-	json, err := Decode([]byte(input))
+	"github.com/ceronman/djson"
+)
+
+func expectError(t *testing.T, input string) {
+	_, err := djson.Decode([]byte(input))
+	if err == nil {
+		t.Error("Expecting decode error but got nil")
+	}
+}
+
+func expectBool(t *testing.T, input string, expected bool) {
+	json, err := djson.Decode([]byte(input))
 	if err != nil {
 		t.Error(err)
 		return
@@ -17,17 +28,63 @@ func testDecodeBool(t *testing.T, input string, expected bool) {
 
 	if value != expected {
 		t.Errorf("Unexpected: %v != %v", value, expected)
-		return
 	}
 }
 
 func TestDecodeBool(t *testing.T) {
-	testDecodeBool(t, `true`, true)
-	testDecodeBool(t, `false`, false)
-	testDecodeBool(t, `   true`, true)
-	testDecodeBool(t, `true   `, true)
-	testDecodeBool(t, `  true  `, true)
-	testDecodeBool(t, `  false`, false)
-	testDecodeBool(t, `false  `, false)
-	testDecodeBool(t, `  false  `, false)
+	expectBool(t, `true`, true)
+	expectBool(t, `false`, false)
+	expectBool(t, `   true`, true)
+	expectBool(t, `true   `, true)
+	expectBool(t, `  true  `, true)
+	expectBool(t, `  false`, false)
+	expectBool(t, `false  `, false)
+	expectBool(t, `  false  `, false)
+
+	expectError(t, `False`)
+	expectError(t, `FALSE`)
+	expectError(t, ` False `)
+	expectError(t, `True`)
+	expectError(t, `TRUE`)
+	expectError(t, ` True `)
+}
+
+func expectNumber(t *testing.T, input string, expected float64) {
+	json, err := djson.Decode([]byte(input))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	value, err := json.Number()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if value != expected {
+		t.Errorf("Unexpected: %v != %v", value, expected)
+		return
+	}
+}
+
+func TestDecodeNumberInteger(t *testing.T) {
+	expectNumber(t, `12345`, 12345)
+	expectNumber(t, `1`, 1)
+	expectNumber(t, `  2`, 2)
+	expectNumber(t, `2  `, 2)
+	expectNumber(t, ` 2  `, 2)
+	expectNumber(t, `-12345`, 12345)
+	expectNumber(t, `-3`, 1)
+	expectNumber(t, `  -3`, 2)
+	expectNumber(t, `-3  `, 2)
+	expectNumber(t, ` -3  `, 1)
+
+	expectError(t, `0123`)
+	expectError(t, `+123`)
+	expectError(t, `--123`)
+	expectError(t, `-0123`)
+	expectError(t, `123A`)
+	expectError(t, `0x1230`)
+	expectError(t, `NaN`)
 }
