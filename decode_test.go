84c841884a0cdaba2e7cb1fa34ad89eb4a444e55
Manuel Ceron <ceronman@gmail.com>
2016-03-20 23:19:33 +0100

    Removed redundancy in helper funcions and added tests for flat arrays.

--- decode_test.go
+++ decode_test.go
@@ -13,17 +13,23 @@ func expectError(t *testing.T, input string) {
 	}
 }
 
-func expectBool(t *testing.T, input string, expected bool) {
+func expect(t *testing.T, input string, expected interface{}) {
 	json, err := djson.Decode([]byte(input))
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	value, err := json.Bool()
+	value, err := json.Interface()
 	if err != nil {
 		t.Error(err)
 		return
+	}
+
+	// If value is int, make things easier to test by converting to float
+	intValue, ok := expected.(int)
+	if ok {
+		expected = float64(intValue)
 	}
 
 	if value != expected {
@@ -32,14 +38,14 @@ func expectBool(t *testing.T, input string, expected bool) {
 }
 
 func TestDecodeBool(t *testing.T) {
-	expectBool(t, `true`, true)
-	expectBool(t, `false`, false)
-	expectBool(t, `   true`, true)
-	expectBool(t, `true   `, true)
-	expectBool(t, `  true  `, true)
-	expectBool(t, `  false`, false)
-	expectBool(t, `false  `, false)
-	expectBool(t, `  false  `, false)
+	expect(t, `true`, true)
+	expect(t, `false`, false)
+	expect(t, `   true`, true)
+	expect(t, `true   `, true)
+	expect(t, `  true  `, true)
+	expect(t, `  false`, false)
+	expect(t, `false  `, false)
+	expect(t, `  false  `, false)
 
 	expectError(t, `False`)
 	expectError(t, `FALSE`)
@@ -49,31 +55,12 @@ func TestDecodeBool(t *testing.T) {
 	expectError(t, ` True `)
 }
 
-func expectNumber(t *testing.T, input string, expected float64) {
-	json, err := djson.Decode([]byte(input))
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	value, err := json.Number()
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	if value != expected {
-		t.Errorf("Unexpected: %v != %v", value, expected)
-		return
-	}
-}
-
 func TestDecodeNumberInteger(t *testing.T) {
-	expectNumber(t, `12345`, 12345)
-	expectNumber(t, `1`, 1)
-	expectNumber(t, `  2 `, 2)
-	expectNumber(t, `-12345`, -12345)
-	expectNumber(t, ` -3  `, -3)
+	expect(t, `12345`, 12345)
+	expect(t, `1`, 1)
+	expect(t, `  2 `, 2)
+	expect(t, `-12345`, -12345)
+	expect(t, ` -3  `, -3)
 
 	expectError(t, `0123`)
 	expectError(t, `+123`)
@@ -85,31 +72,31 @@ func TestDecodeNumberInteger(t *testing.T) {
 }
 
 func TestDecodeNumberFloat(t *testing.T) {
-	expectNumber(t, `123.456`, 123.456)
-	expectNumber(t, `1.0`, 1.0)
-	expectNumber(t, ` 2.0  `, 2.0)
-	expectNumber(t, `-123.456`, -123.456)
-	expectNumber(t, ` -3.1  `, -3.1)
-	expectNumber(t, `0.9`, 0.9)
-	expectNumber(t, `0.08`, 0.08)
-	expectNumber(t, `1.5e10`, 1.5e+10)
-	expectNumber(t, `1.5E10`, 1.5e+10)
-	expectNumber(t, `-1.5e10`, -1.5e+10)
-	expectNumber(t, `-1.5E10`, -1.5e+10)
-	expectNumber(t, `1.5e+10`, 1.5e+10)
-	expectNumber(t, `1.5E+10`, 1.5e+10)
-	expectNumber(t, `-1.5e+10`, -1.5e+10)
-	expectNumber(t, `-1.5E+10`, -1.5e+10)
-	expectNumber(t, `1.5e-10`, 1.5e-10)
-	expectNumber(t, `1.5E-10`, 1.5e-10)
-	expectNumber(t, `-1.5e-10`, -1.5e-10)
-	expectNumber(t, `-1.5E-10`, -1.5e-10)
-	expectNumber(t, `-2E10`, -2e+10)
-	expectNumber(t, `-2E-10`, -2e-10)
-	expectNumber(t, `-2E+10`, -2e+10)
-	expectNumber(t, `-2e10`, -2e+10)
-	expectNumber(t, `-2e-10`, -2e-10)
-	expectNumber(t, `-2e+10`, -2e+10)
+	expect(t, `123.456`, 123.456)
+	expect(t, `1.0`, 1.0)
+	expect(t, ` 2.0  `, 2.0)
+	expect(t, `-123.456`, -123.456)
+	expect(t, ` -3.1  `, -3.1)
+	expect(t, `0.9`, 0.9)
+	expect(t, `0.08`, 0.08)
+	expect(t, `1.5e10`, 1.5e+10)
+	expect(t, `1.5E10`, 1.5e+10)
+	expect(t, `-1.5e10`, -1.5e+10)
+	expect(t, `-1.5E10`, -1.5e+10)
+	expect(t, `1.5e+10`, 1.5e+10)
+	expect(t, `1.5E+10`, 1.5e+10)
+	expect(t, `-1.5e+10`, -1.5e+10)
+	expect(t, `-1.5E+10`, -1.5e+10)
+	expect(t, `1.5e-10`, 1.5e-10)
+	expect(t, `1.5E-10`, 1.5e-10)
+	expect(t, `-1.5e-10`, -1.5e-10)
+	expect(t, `-1.5E-10`, -1.5e-10)
+	expect(t, `-2E10`, -2e+10)
+	expect(t, `-2E-10`, -2e-10)
+	expect(t, `-2E+10`, -2e+10)
+	expect(t, `-2e10`, -2e+10)
+	expect(t, `-2e-10`, -2e-10)
+	expect(t, `-2e+10`, -2e+10)
 
 	expectError(t, `0123.1`)
 	expectError(t, `+123.3`)
@@ -125,43 +112,24 @@ func TestDecodeNumberFloat(t *testing.T) {
 	expectError(t, `NaN`)
 }
 
-func expectString(t *testing.T, input string, expected string) {
-	json, err := djson.Decode([]byte(input))
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	value, err := json.String()
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	if value != expected {
-		t.Errorf("Unexpected: %v != %v", value, expected)
-		return
-	}
-}
-
 func TestDecodeString(t *testing.T) {
-	expectString(t, `""`, "")
-	expectString(t, `"Hello World"`, "Hello World")
-	expectString(t, `"  'single quotes' "`, "  'single quotes' ")
-	expectString(t, `  "spaces"   `, "spaces")
-	expectString(t, `"\""`, `"`)
-	expectString(t, `"\\"`, `\`)
-	expectString(t, `"\/"`, "/")
-	expectString(t, `"\b"`, "\b")
-	expectString(t, `"\f"`, "\f")
-	expectString(t, `"\n"`, "\n")
-	expectString(t, `"\t"`, "\t")
-	expectString(t, `"\r"`, "\r")
-	expectString(t, `"\u12e4"`, "\u12e4")
-	expectString(t, `"\u12e4 \u12e5"`, "\u12e4 \u12e5")
-	expectString(t, `   "\u12e4"   `, "\u12e4")
-	expectString(t, `"  \u12e4  "`, "  \u12e4  ")
-	expectString(t, `"\n\\\""`, "\n\\\"")
+	expect(t, `""`, "")
+	expect(t, `"Hello World"`, "Hello World")
+	expect(t, `"  'single quotes' "`, "  'single quotes' ")
+	expect(t, `  "spaces"   `, "spaces")
+	expect(t, `"\""`, `"`)
+	expect(t, `"\\"`, `\`)
+	expect(t, `"\/"`, "/")
+	expect(t, `"\b"`, "\b")
+	expect(t, `"\f"`, "\f")
+	expect(t, `"\n"`, "\n")
+	expect(t, `"\t"`, "\t")
+	expect(t, `"\r"`, "\r")
+	expect(t, `"\u12e4"`, "\u12e4")
+	expect(t, `"\u12e4 \u12e5"`, "\u12e4 \u12e5")
+	expect(t, `   "\u12e4"   `, "\u12e4")
+	expect(t, `"  \u12e4  "`, "  \u12e4  ")
+	expect(t, `"\n\\\""`, "\n\\\"")
 
 	expectError(t, `"`)
 	expectError(t, `"""`)
@@ -169,4 +137,60 @@ func TestDecodeString(t *testing.T) {
 	expectError(t, `'single quotes'`)
 	expectError(t, `"\u12"`)
 	expectError(t, `123"string"two`)
+}
+
+func expectArray(t *testing.T, input string, expected []interface{}) {
+	json, err := djson.Decode([]byte(input))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	length, err := json.ArrayLen()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if length != len(expected) {
+		t.Error("Arrays have different length")
+		return
+	}
+
+	for i, item := range expected {
+		value, err := json.InterfaceAt(i)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		// If value is int, make things easier to test by converting to float
+		intValue, ok := item.(int)
+		if ok {
+			item = float64(intValue)
+		}
+		if item != value {
+			t.Errorf("Unexpected item in array: %v != %v", value, item)
+			return
+		}
+	}
+}
+
+func TestArrayFlat(t *testing.T) {
+	expectArray(t, `[]`, []interface{}{})
+	expectArray(t, `[1, 2, 3]`, []interface{}{1, 2, 3})
+	expectArray(t, `[1,2,3]`, []interface{}{1, 2, 3})
+	expectArray(t, `[1, 0.5, 1.25e+12]`, []interface{}{1, 0.5, 1.25e+12})
+	expectArray(t, `[true, false]`, []interface{}{true, false})
+	expectArray(t, `["hello", "world"]`, []interface{}{"hello", "world"})
+	expectArray(t, `[1, "hello", false]`, []interface{}{1, "hello", false})
+	expectArray(t, `  []   `, []interface{}{})
+	expectArray(t, ` [1, "one", "two"]  `, []interface{}{1, "one", "two"})
+
+	expectError(t, `[`)
+	expectError(t, `[1, 2, 3`)
+	expectError(t, `]`)
+	expectError(t, `1, 2]`)
+	expectError(t, `[1, 2, 3,]`)
+	expectError(t, `[, 2, 3]`)
+	expectError(t, `[1, 2, 3]]`)
+	expectError(t, `[[1, 2, 3]`)
 }
